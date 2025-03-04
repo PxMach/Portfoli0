600df1f636ebfe775958b023c20d4bb228e0b42a
PxMach <moundjidmachghour58@gmail.com>
2025-03-04 11:34:06 +0100

    Add dark theme functionality with theme toggle icon

--- script.js
+++ script.js
@@ -1,13 +1,26 @@
 document.querySelectorAll("nav a").forEach((anchor) => {
-  anchor.addEventListener("click", function (e) {
-    e.preventDefault();
-    const targetId = this.getAttribute("href").substring(1);
-    document.getElementById(targetId).scrollIntoView({
-      behavior: "smooth",
-    });
-  });
+   anchor.addEventListener("click", function (e) {
+      e.preventDefault();
+      const targetId = this.getAttribute("href").substring(1);
+      document.getElementById(targetId).scrollIntoView({
+         behavior: "smooth",
+      });
+   });
 });
 
 document.querySelector(".hamburger").addEventListener("click", function () {
-  document.querySelector("nav").classList.toggle("active");
+   document.querySelector("nav").classList.toggle("active");
 });
+
+let icon = document.getElementById("icon");
+
+function changeTheme() {
+   icon.addEventListener("click", function () {
+      document.body.classList.toggle("dark-theme");
+      if (document.body.classList.contains("dark-theme")) {
+         icon.src = "images/dark theme icon/sun.png";
+      } else {
+         icon.src = "images/dark theme icon/moon.png";
+      }
+   });
+}
